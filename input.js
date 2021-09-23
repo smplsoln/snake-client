ac54c5424daa7608438477656b7f533bfe2824dc
Prashant R <smplsoln@gmail.com>
2021-09-22 20:32:36 -0600

    Add WASD input handling

--- input.js
+++ input.js
@@ -1,4 +1,44 @@
+
+const moves = ["Move: up", 'Move: left', 'Move: down', 'Move: right'];
+
+let connection;
+
+const getRandomMove = () => {
+  const moveIndex = randomIndex(moves.length);
+  console.log('moveIndex: ' + moveIndex);
+  const move = moves[moveIndex];
+  // console.log('move ' + moveIndex + ' : ' + move);
+  return move;
+};
+
+const randomIndex = (bound) => {
+  return Math.floor(Math.floor(Math.random() * 1000) % bound);
+};
+
+const getMoveForKey = (key) => {
+  let moveIndex = 0;
+  if (key === 'w' || key === 'W') {
+    moveIndex = 0;
+  }
+
+  if (key === 'a' || key === 'A') {
+    moveIndex = 1;
+  }
+
+  if (key === 's' || key === 'S') {
+    moveIndex = 2;
+  }
+
+  if (key === 'd' || key === 'D') {
+    moveIndex = 3;
+  }
+  let move = moves[moveIndex];
+  return move;
+};
+
+let moveCount = 0;
 const handleUserInput = (key) => {
+  console.log(`Key pressed: ${key}`);
   // your code here
   // handle CTRL+C
   if (key === '\u0003') {
@@ -6,9 +46,21 @@ const handleUserInput = (key) => {
     // conn.destroy();
     process.exit();
   }
+  if (connection.destroyed) {
+    console.log('Connection destroyed: Not sending Move commands to server');
+    process.exit();
+    return;
+  }
+
+  let move = getMoveForKey(key);
+  console.log(`move ${moveCount} : ${move}`);
+  moveCount++;
+  connection.write(move);
 };
 
-const setupInput = () => {
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -17,4 +69,5 @@ const setupInput = () => {
   return stdin;
 };
 
-module.exports = { setupInput }
+module.exports = { setupInput, getRandomMove };
+
--- play.js
+++ play.js
@@ -1,17 +1,16 @@
 // const net = require('net');
 
 const { connect } = require('./client');
-const { setupInput } = require("./input");
+const { setupInput, getRandomMove } = require("./input");
 
 // establish a connection with the game server
 // const connect = client.connect;
 
 console.log({ connect });
-
 console.log("Connecting ...");
 let conn = connect();
 
-console.log(`Connection obj returneed by node: ${conn}`);
+console.log(`Connection obj returned by node: ${conn}`);
 console.log({ conn });
 
 conn.on("connect", () => {
@@ -23,7 +22,7 @@ conn.on("connect", () => {
   conn.write("Name: PAR");
 });
 
-// listener when data is receeived from server
+// listener when data is received from server
 conn.on('data', (data) => {
   console.log(`Server ~~~: ${data}`);
 });
@@ -44,41 +43,27 @@ conn.on('timeout', () => {
 });
 
 // Setup Standard Input ( and keyboard listener)
-const stdin = setupInput();
+const stdin = setupInput(conn);
 
 // Moves
-const moveInterval = 50;
-const moves = ["Move: up", 'Move: left', 'Move: down', 'Move: right'];
+// let moveInterval = 50;
+// let moveCount = 0;
+// let refreshIntervalId;
 
-const getRandomMove = () => {
-  const moveIndex = randomIndex(moves.length);
-  console.log('moveIndex: ' + moveIndex);
-  const move = moves[moveIndex];
-  // console.log('move ' + moveIndex + ' : ' + move);
-  return move;
-};
-
-const randomIndex = (bound) => {
-  return Math.floor(Math.floor(Math.random() * 1000) % bound);
-};
-
-let moveCount = 0;
-
-let refreshIntervalId;
 // 2nd callback on 'connect' to send moves
-conn.on("connect", () => {
-  console.log("*** Connected to Snek server! ***");
-  refreshIntervalId = setInterval(() => {
-    if (conn.destroyed) {
-      console.log('Connection destroyed: Not sending Move commands to server');
-      console.log(`Connection destroyed, Ending client data sender: ${refreshIntervalId}`);
-      clearInterval(refreshIntervalId);
-      process.exit();
-      return;
-    }
-    const move = getRandomMove();
-    console.log(`move ${moveCount} : ${move}`);
-    moveCount++;
-    conn.write(move);
-  }, moveInterval);
-});
+// conn.on("connect", () => {
+//   console.log("*** Connected to Snek server! ***");
+//   refreshIntervalId = setInterval(() => {
+//     if (conn.destroyed) {
+//       console.log('Connection destroyed: Not sending Move commands to server');
+//       console.log(`Connection destroyed, Ending client data sender: ${refreshIntervalId}`);
+//       clearInterval(refreshIntervalId);
+//       process.exit();
+//       return;
+//     }
+//     const move = getRandomMove();
+//     console.log(`move ${moveCount} : ${move}`);
+//     moveCount++;
+//     conn.write(move);
+//   }, moveInterval);
+// });
